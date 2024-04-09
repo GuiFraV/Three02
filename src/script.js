208cb781707799c1316882904829ed57b054dc14
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-09 10:44:00 +0200

    Update mesh position, scale, and rotation, add axes helper, and adjust camera position in script.js

--- src/script.js
+++ src/script.js
@@ -13,12 +13,9 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 
-
-
 // mesh.position.x = 0.7
 // mesh.position.y = -0.6
 // mesh.position.z = -1
-
 
 mesh.position.set(0.7, -0.4, -1)
 
@@ -27,9 +24,11 @@ mesh.position.set(0.7, -0.4, -1)
 // mesh.scale.z = 0.5
 
 mesh.scale.set(2, 0.5, 0.5)
-
-
 scene.add(mesh)
+
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = Math.PI* 0.25
+mesh.rotation.z = Math.PI* 0.25
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(3)
