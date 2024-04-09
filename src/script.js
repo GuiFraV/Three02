5790bbdfc81ed0d27ba41e3e29429d50665dca42
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-09 10:50:21 +0200

    Adjust camera position in script.js

--- src/script.js
+++ src/script.js
@@ -48,11 +48,11 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.y = 1
-camera.position.x = 1
+// camera.position.y = 1
+// camera.position.x = 1
 scene.add(camera)
 
-console.log(mesh.position.length())
+camera.lookAt(mesh.position)
 
 
 /**
