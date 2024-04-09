55586460ccb8bbbb53f9a95896d600adc637e805
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-09 10:59:08 +0200

    Refactor mesh position, scale, and rotation, add axes helper, and adjust camera position in script.js

--- src/script.js
+++ src/script.js
@@ -9,29 +9,41 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = -1
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color : 0xff0000 }),
+)
 
-mesh.position.set(0.7, -0.4, -1)
+group.add(cube1)
 
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color : 0x00ff00 }),
+)
 
-mesh.scale.set(2, 0.5, 0.5)
-scene.add(mesh)
+cube2.position.x =-2
 
-mesh.rotation.reorder('YXZ')
-mesh.rotation.y = Math.PI* 0.25
-mesh.rotation.z = Math.PI* 0.25
+group.add(cube2)
+
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color : 0x0000ff }),
+)
+
+cube3.position.x =2
+
+group.add(cube3)
+
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper(3)
+const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
 
 
@@ -51,9 +63,6 @@ camera.position.z = 3
 // camera.position.y = 1
 // camera.position.x = 1
 scene.add(camera)
-
-camera.lookAt(mesh.position)
-
 
 /**
  * Renderer
